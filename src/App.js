d5317c1fc17755ac7d971374bc90ec5970b775f8
suelytohm <suelytohm.02@gmail.com>
2022-08-25 22:43:14 -0300

    request usando axios e fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+const axios = require('axios').default;
+
 
 function App() {
+
+  const [products, setProducts] = useState([]);
+  const url = "http://localhost:3000/products";
+
+  // Request usando axios
+  useEffect(() => {
+    (async () => {
+      await axios.get(url).then(function (response) {
+        console.log(response.data);
+    
+    
+      }).catch(function (error) {
+        console.log(error)
+      })
+    })();
+
+  }, []);
+
+  
+
+
+  // Request usando fetch
+
+  useEffect(() => {
+
+    (async () => {
+      const res = await fetch(url);
+  
+      const data = await res.json();
+  
+      setProducts(data);
+
+    })();
+  }, []);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">
+        <h1>Lista de produtos</h1>
+      </div>
+      <div>
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>
+              <div key={product.id} className="card">
+                <h3 className="product-title">
+                  {product.name}
+                </h3>
+                <p className="product-price">
+                  R${product.price.toFixed(2)}
+                </p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+
     </div>
   );
 }
